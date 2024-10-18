727b42d6a5042380a0a0ecf7a2e5f440586ef98f
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-10-18 16:32:45 +0300

    .

--- src/test/java/pages/ParentPage.java
+++ src/test/java/pages/ParentPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 import utilities.GWD;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
@@ -8,6 +10,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.time.Duration;
+import java.util.List;
 
 public class ParentPage {
     public WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(10));
@@ -30,9 +33,33 @@ public class ParentPage {
         js.executeScript("arguments[0].scrollIntoView();", elements);
     }
 
-    public void LoginContainsText(WebElement element, String value) {
+    public void verifyContainsText(WebElement element, String value) {
         wait.until(ExpectedConditions.textToBePresentInElement(element, value));
         Assert.assertTrue(element.getText().toLowerCase().contains(value.toLowerCase()));
+        new Actions(GWD.getDriver()).sendKeys(Keys.ESCAPE).build().perform();
+    }
+
+    public void jsClick(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        JavascriptExecutor js = (JavascriptExecutor) GWD.getDriver();
+        js.executeScript("arguments[0].click();", element);
+    }
+
+    public static boolean listContainsString(List<WebElement> list, String search) {
+        boolean isFound = false;
+        for (WebElement e : list) {
+            if (e.getText().equalsIgnoreCase(search))
+                isFound = true;
+        }
+        return isFound;
+    }
+
+    public void wait(int sn) {
+        try {
+            Thread.sleep(sn * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
