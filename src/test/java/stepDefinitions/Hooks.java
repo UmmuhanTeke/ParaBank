b9271b25419dff313b20fd3c0d5cf21998673fb9
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-10-18 17:36:48 +0300

    .

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,12 +1,21 @@
 package stepDefinitions;
 
 import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import utilities.GWD;
 
 public class Hooks {
 
     @After
-    public void after (){
+    public void after(Scenario scenario) {
+
+        if (scenario.isFailed()) {
+            TakesScreenshot ts = ((TakesScreenshot) GWD.getDriver());
+            byte[] hafizadakiHali = ts.getScreenshotAs(OutputType.BYTES);
+            scenario.attach(hafizadakiHali, "image/png", "screenshot name");
+        }
         GWD.tearDown();
     }
 }
