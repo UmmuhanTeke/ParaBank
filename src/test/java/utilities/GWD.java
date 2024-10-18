d3382a43b84766289ca0cda95bae536fcb8f8da3
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-10-19 02:00:43 +0300

    .

--- src/test/java/utilities/GWD.java
+++ src/test/java/utilities/GWD.java
@@ -2,58 +2,42 @@ package utilities;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 import java.util.Locale;
 
 public class GWD {
 
-    private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
-    public static ThreadLocal<String> threadBrowserName = new ThreadLocal<>();
+    private static WebDriver driver;
 
-    public static WebDriver getDriver() {
+    public static WebDriver getDriver()
+    {
         Locale.setDefault(new Locale("EN"));
         System.setProperty("user.language", "EN");
 
-        if (threadBrowserName.get() == null)
-            threadBrowserName.set("chrome");
-
-        if (threadDriver.get() == null) {
-            switch (threadBrowserName.get()) {
-                case "firefox":
-                    threadDriver.set(new FirefoxDriver());
-                    break;
-                case "edge":
-                    threadDriver.set(new EdgeDriver());
-                    break;
-                default:
-                    threadDriver.set(new ChromeDriver());
-                    break;
-            }
-
-            threadDriver.get().manage().window().maximize();
-            threadDriver.get().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(ConfigReader.getIntProperty("pageLoadTimeout")));
+        if (driver == null) //hiç oluşturulmamış ise
+        {
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(ConfigReader.getIntProperty("pageLoadTimeout")));
         }
-        return threadDriver.get();
+        return driver;
     }
 
-    public static void tearDown() {
-
+    public static void tearDown(){
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        if (threadDriver.get() != null) {
-            threadDriver.get().quit();
-
-            WebDriver driver = threadDriver.get();
-            driver = null;
-            threadDriver.set(driver);
+        if (driver != null)
+        {
+            driver.quit();
+            driver=null;
         }
     }
 }
+
+
 
