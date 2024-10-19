ff464f965a6d70c3a25029350b4430f7c95dfdc1
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-10-20 02:36:58 +0300

    .

--- src/test/java/utilities/ConfigReader.java
+++ src/test/java/utilities/ConfigReader.java
@@ -1,11 +1,17 @@
 package utilities;
 
+import com.github.javafaker.Faker;
+
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Properties;
 
 public class ConfigReader {
      private static Properties properties = new Properties();
+     static Faker faker=new Faker(new Locale("en-US"));
 
     static {
         try {
@@ -24,6 +30,33 @@ public class ConfigReader {
 
     public static int getIntProperty(String key) {
         return Integer.parseInt(properties.getProperty(key));
+    }
+
+    public static void updateProperty(String keyword){
+
+        switch (keyword){
+            case "username":
+                String name=faker.name().username();
+                properties.setProperty(keyword,name);
+                break;
+            case "password":
+                String password=faker.internet().password();
+                properties.setProperty(keyword,password);
+                break;
+        }
+
+        FileOutputStream outputFile=null;
+        try {
+            outputFile=new FileOutputStream("configuration.properties");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            properties.store(outputFile ,null);
+            outputFile.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
