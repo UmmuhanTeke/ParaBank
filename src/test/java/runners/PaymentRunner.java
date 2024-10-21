ad8da8876771bf7049d4319773ba0a7526214aad
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-10-21 21:52:45 +0300

    Addition made.

--- src/test/java/runners/PaymentRunner.java
+++ src/test/java/runners/PaymentRunner.java
@@ -1,4 +1,14 @@
 package runners;
 
-public class PaymentRunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        tags = "@Payment",
+        features = {"src/test/java/featureFiles"},
+        glue = {"stepDefinitions"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+)
+
+public class PaymentRunner extends AbstractTestNGCucumberTests {
 }
