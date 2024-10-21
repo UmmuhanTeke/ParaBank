ab1d8949b5a0a464a034a6fbbd23f4eea6c17c65
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-10-21 21:50:31 +0300

    Addition made

--- src/test/java/runners/SmokeRunner.java
+++ src/test/java/runners/SmokeRunner.java
@@ -1,4 +1,15 @@
 package runners;
 
-public class SmokeRunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        tags = "@Smoke",
+        features = {"src/test/java/featureFiles"},
+        glue = {"stepDefinitions"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+
+)
+
+public class SmokeRunner extends AbstractTestNGCucumberTests {
 }
