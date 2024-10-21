cbdd89d68913e8c261b52e0e6a46e132df0277fd
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-10-21 21:49:01 +0300

    Additions were made

--- src/test/java/runners/RegressionRunner.java
+++ src/test/java/runners/RegressionRunner.java
@@ -1,4 +1,14 @@
 package runners;
 
-public class RegressionRunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        tags = "@Regression",
+        features = {"src/test/java/featureFiles"},
+        glue = {"stepDefinitions"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+)
+
+public class RegressionRunner extends AbstractTestNGCucumberTests{
 }
