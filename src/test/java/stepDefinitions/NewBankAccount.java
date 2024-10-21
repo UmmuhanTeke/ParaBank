3a8e4d363d3292d0c1a8ee89d2a6124942dbaf13
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-10-21 22:43:29 +0300

    Update NewBankAccount.java

--- src/test/java/stepDefinitions/NewBankAccount.java
+++ src/test/java/stepDefinitions/NewBankAccount.java
@@ -5,30 +5,28 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class NewBankAccount {
-    @When("Click on the Element in Content")
+   @When("Click on the Element in Content")
     public void clickOnTheElementInContent() {
-        
     }
-
 
     @And("The user is directed to the bank account creating page")
     public void theUserIsDirectedToTheBankAccountCreatingPage() {
-        
     }
 
-    @And("The user selects the account type from the drop-down menu")
-    public void theUserSelectsTheAccountTypeFromTheDropDownMenu() {
-        
+    @And("The user selects the {string} type from the drop-down menu")
+    public void theUserSelectsTheTypeFromTheDropDownMenu(String arg0) {
     }
 
     @And("The user receives a warning that the new bank account should have a minimum balance")
     public void theUserReceivesAWarningThatTheNewBankAccountShouldHaveAMinimumBalance() {
-        
     }
 
-    @And("The user selects the account from the Drop-Down menu")
-    public void theUserSelectsTheAccountFromTheDropDownMenu() {
-        
+    @And("The user selects the accountNumber from the Drop-Down menu")
+    public void theUserSelectsTheAccountNumberFromTheDropDownMenu() {
+    }
+
+    @And("Click on the Element in Button")
+    public void clickOnTheElementInButton() {
     }
 
     @Then("The user confirms the successful creation of the new bank account")
