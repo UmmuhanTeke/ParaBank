1b29bb73b7feaaf011deb280252d346a2c16c84b
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-10-23 16:21:15 +0300

    Contact Update Info class added.

---
+++ src/test/java/stepDefinitions/ContactUpdateInfo.java
@@ -0,0 +1,56 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ContactUpdateInfo {
+    @When("The user clicks on the Update Contact Info")
+    public void theUserClicksOnTheUpdateContactInfo() {
+
+    }
+
+    @Then("The user accesses the update profile page")
+    public void theUserAccessesTheUpdateProfilePage() {
+    }
+
+    @And("The user views their information")
+    public void theUserViewsTheirInformation() {
+    }
+
+    @And("The user updates the information they want")
+    public void theUserUpdatesTheInformationTheyWant() {
+    }
+
+    @When("The user clicks on the Update Profile button")
+    public void theUserClicksOnTheUpdateProfileButton() {
+    }
+
+    @Then("The user verifies the confirmation message on the Profile Updated page")
+    public void theUserVerifiesTheConfirmationMessageOnTheProfileUpdatedPage() {
+    }
+
+    @And("The user clicks on the Log Out button")
+    public void theUserClicksOnTheLogOutButton() {
+    }
+
+    @When("The user logs in to the site again")
+    public void theUserLogsInToTheSiteAgain() {
+    }
+
+    @Then("The user clicks on the Update Contact Info button")
+    public void theUserClicksOnTheUpdateContactInfoButton() {
+    }
+
+    @And("The user verifies the updated version of their contact information")
+    public void theUserVerifiesTheUpdatedVersionOfTheirContactInformation() {
+    }
+
+    @And("The user deletes at least three optional fields from their contact information and leaves them blank")
+    public void theUserDeletesAtLeastThreeOptionalFieldsFromTheirContactInformationAndLeavesThemBlank() {
+    }
+
+    @Then("The user verifies the warning messages for the fields left blank")
+    public void theUserVerifiesTheWarningMessagesForTheFieldsLeftBlank() {
+    }
+}
