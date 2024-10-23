5bb2933503e5bffb41fecdc68dd404623c63530f
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-10-24 00:22:12 +0300

    Contact Update Info class added.

--- src/test/java/pages/LeftNav.java
+++ src/test/java/pages/LeftNav.java
@@ -159,95 +159,54 @@ public class LeftNav extends ParentPage{
 
     //----------------------------ÜMMÜHAN-----------------------------------
 
-    @FindBy(xpath = "//a[text()='Open New Account']")
-    public WebElement openNewAccount;
 
-    @FindBy(xpath = "(//*[text()='Open New Account'])[2]")
-    public WebElement newAccountPageControl;
 
-    @FindBy(xpath = "//*[text()='CHECKING']")
-    public WebElement selectChecking;
 
-    @FindBy(xpath = "//*[text()='SAVINGS']")
-    public WebElement selectSavings;
 
-    @FindBy(xpath = "//select[@id='type']")
-    public WebElement selectMenu;
 
-    @FindBy(xpath = "(//div[@id='openAccountForm']/form/p/b)[2]")
-    public WebElement minBalanceText;
 
-    @FindBy(xpath = "//input[@class='button']")
-    public WebElement openNewAccountButton;
 
-    @FindBy(xpath = "//div[@id='openAccountResult']//p")
-    public WebElement accountOpenedText;
 
-    @FindBy(id = "newAccountId")
-    public WebElement accountNumberClick;
 
-    @FindBy(xpath = "(//div[@id='accountDetails']/table/tbody/tr/td)[2]")
-    public WebElement accountDetailsNumber;
 
-    @FindBy(xpath = "(//div[@id='accountDetails']/table/tbody/tr/td)[4]")
-    public WebElement accountName;
 
-    @FindBy(xpath = "//select[@id='fromAccountId']/option")
-    public List<WebElement> selectAccountNumber;
 
-    @FindBy(xpath = "//*[text()='Update Contact Info']")
-    public WebElement updateContactInfoButton;
 
-    @FindBy(xpath = "//div[@id='updateProfileResult']/p")
-    public WebElement updateProfileControl;
 
-    @FindBy(xpath = "//table[@class='form2']/tbody/tr")
-    public List<WebElement> updateProfileDetails;
 
-    @FindBy(xpath = "//*[text()='Log Out']")
-    public WebElement logOutButton;
 
-    @FindBy(xpath = "(//tr/td)[5]/input")
-    public WebElement lastName;
 
-    @FindBy(xpath = "//span[@id='lastName-error']")
-    public WebElement lastNameError;
 
-    @FindBy(xpath = "(//tr/td)[8]/input")
-    public WebElement address;
 
-    @FindBy(xpath = "//span[@id='street-error']")
-    public WebElement addressError;
 
-    @FindBy(xpath = "(//tr/td)[11]/input")
-    public WebElement city;
 
-    @FindBy(xpath = "//span[@id='city-error']")
-    public WebElement cityError;
 
-    @FindBy(xpath = "//input[@name='username']")
-    public WebElement username;
 
-    @FindBy(xpath = "//input[@name='password']")
-    public WebElement password;
 
-    @FindBy(xpath = "//input[@class='button']")
-    public WebElement loginButton;
 
-    @FindBy(xpath = "//input[@type='button']")
-    public WebElement updateProfileButton;
 
-    public WebElement getWebElement(String strWebElement){
-        switch (strWebElement){
-            case "openNewAccount": return this.openNewAccount;
-            case "openNewAccountButton": return this.openNewAccountButton ;
-            case "selectSavings": return this.selectSavings;
-            case "selectChecking": return this.selectChecking;
-            case "updateContactInfoButton": return this.updateContactInfoButton;
-            case "updateProfileButton": return this.updateProfileButton;
-        }
-        return null;
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
