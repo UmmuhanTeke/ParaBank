0c5c51bbe4c52e641dc50db55b611a52cfdf3a23
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-10-24 16:29:55 +0300

    Parallel_AllRunner xml and GWD addition was made into the class.

--- src/test/java/utilities/GWD.java
+++ src/test/java/utilities/GWD.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 import java.util.Locale;
@@ -28,9 +27,6 @@ public class GWD {
                     break;
                 case "edge":
                     threadDriver.set(new EdgeDriver());
-                    break;
-                case "safari":
-                    threadDriver.set(new SafariDriver());
                     break;
                 default:
                     threadDriver.set(new ChromeDriver());
