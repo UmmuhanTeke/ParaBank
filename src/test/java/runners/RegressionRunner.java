82d0991c70c58d1b53771df39b0727ed588c08a2
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-11-01 04:11:30 +0300

    new bank account edited.

--- src/test/java/runners/RegressionRunner.java
+++ src/test/java/runners/RegressionRunner.java
@@ -5,11 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         tags = "@Regression",
-        features = {"src/test/java/featureFiles/Register.feature",
-                "src/test/java/featureFiles/Login.feature",
-                "src/test/java/featureFiles/BillPay.feature",
-                "src/test/java/featureFiles/NewBankAccount.feature",
-                "src/test/java/featureFiles/LoanApplication.feature"},
+        features = {"src/test/java/featureFiles"},
         glue = {"stepDefinitions"},
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
--- src/test/java/runners/SmokeRunner.java
+++ src/test/java/runners/SmokeRunner.java
@@ -5,10 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         tags = "@Smoke",
-        features = {"src/test/java/featureFiles/Register.feature",
-                "src/test/java/featureFiles/Login.feature",
-                "src/test/java/featureFiles/BillPay.feature",
-                "src/test/java/featureFiles/NewBankAccount.feature"},
+        features = {"src/test/java/featureFiles"},
         glue = {"stepDefinitions"},
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
